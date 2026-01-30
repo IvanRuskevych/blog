93e7a141f4c1800feab7954730330d95f6e5cf7a
IvanRuskevych <fullstack.dev.goit@gmail.com>
2026-01-30 16:43:38 +0200

    feat(course): topic 003 View

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,17 @@
 class HomeController < ApplicationController
   before_action :auth
 
+  layout "new_app_layout"
+
   def index
+    @numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 ]
+    # render json: @numbers
+    # render xml: @numbers
+    # render html: @numbers
+    # render plain: @numbers
+
+    # @text = "<h1>test text</h1>".html_safe
+    # @text = "<h1 style='color:red;'><a href='#'>test text</a></h1><script>alert('test alert')</script>".html_safe
+    @text = "<h1><a href='#' style='color:red;'>test text</a></h1><script>alert('test alert')</script>".html_safe
   end
 end
